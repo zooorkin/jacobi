fc3dba7fa5cb463a93f1514f8cd3d5b7b495cc17
Andrew Zooorkin <zooorkin@icloud.com>
2017-12-11 20:44:45 +0300

    Initial Commit

---
+++ crypt03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  crypt03
+//
+//  Created by Андрей Зорькин on 11.12.17.
+//  Copyright © 2017 Андрей Зорькин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
